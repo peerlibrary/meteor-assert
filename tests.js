6a840248036cf030994cfb23f1f7d81baba1d600
Mitar <mitar.git@tnode.com>
2013-06-28 15:23:34 -0700

    Test for Travis CI.

--- tests.js
+++ tests.js
@@ -10,4 +10,12 @@ Tinytest.add('meteor-assert', function (test) {
   test.throws(function () {
     assert.equal('a', 'b');
   });
+  if (Meteor.isServer) {
+    console.log("Meteor.isServer");
+    test.equal(1, 2);
+  }
+  else {
+    console.log("Meteor.isClient");
+    test.equal(1, 3);
+  }
 });

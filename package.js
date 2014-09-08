8eaccc395fcdb6865cdc79c171138e98749d6656
Mitar <mitar.git@tnode.com>
2014-09-08 13:37:40 -0700

    Migration to Meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  summary: "Make code assertions to assure your code works as expected"
+  summary: "Make code assertions to assure your code works as expected",
+  version: '0.2.5',
+  name: 'peerlibrary:assert',
+  git: 'https://github.com/peerlibrary/meteor-assert.git'
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.1');
   api.export('assert');
 
   api.add_files([
@@ -16,6 +20,6 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use(['assert', 'tinytest', 'test-helpers'], ['client', 'server']);
+  api.use(['peerlibrary:assert', 'tinytest', 'test-helpers'], ['client', 'server']);
   api.add_files('tests.js', ['client', 'server']);
 });

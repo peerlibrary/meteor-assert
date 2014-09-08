6a52d8feea574171c0755746eb361ff3226c653a
Mitar <mitar.git@tnode.com>
2014-09-08 13:54:37 -0700

    Fixed tests.

--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@ Tinytest.add('assert', function (test) {
   catch (e) {
   }
   test.isTrue(isDefined, "assert is not defined");
-  test.isTrue(Package.assert.assert, "Package.assert.assert is not defined");
+  test.isTrue(Package['peerlibrary:assert'].assert, "Package.peerlibrary:assert.assert is not defined");
 
   test.throws(function () {
     assert.equal('a', 'b', 'message');

126fc97598ab01afa099d66db2bbdd9492fb6239
Mitar <mitar.git@tnode.com>
2014-09-08 13:40:23 -0700

    Transitory package.

--- export-assert.js
+++
@@ -1,1 +0,0 @@
-assert = this.assert;
--- package.js
+++ package.js
@@ -1,25 +1,10 @@
 Package.describe({
   summary: "Make code assertions to assure your code works as expected",
   version: '0.2.5',
-  name: 'peerlibrary:assert',
+  name: 'mrt:assert',
   git: 'https://github.com/peerlibrary/meteor-assert.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.export('assert');
-
-  api.add_files([
-    'server.js'
-  ], 'server');
-
-  api.add_files([
-    'client/assert.js',
-    'export-assert.js'
-  ], 'client');
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:assert', 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests.js', ['client', 'server']);
+  api.imply('peerlibrary:assert@0.2.5');
 });

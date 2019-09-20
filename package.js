0ce76ad0587d5ee5f6800d65e72e766950a05dd8
Mitar <mitar.git@tnode.com>
2019-09-19 19:09:39 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,25 +1,35 @@
 Package.describe({
   summary: "Make code assertions to assure your code works as expected",
-  version: '0.2.5',
+  version: '0.3.0',
   name: 'peerlibrary:assert',
   git: 'https://github.com/peerlibrary/meteor-assert.git'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.8.1');
+
   api.export('assert');
 
-  api.add_files([
+  api.addFiles([
     'server.js'
   ], 'server');
 
-  api.add_files([
+  api.addFiles([
     'client/assert.js',
     'export-assert.js'
   ], 'client');
 });
 
-Package.on_test(function (api) {
-  api.use(['peerlibrary:assert', 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests.js', ['client', 'server']);
+Package.onTest(function (api) {
+  api.versionsFrom('METEOR@1.8.1');
+
+  api.use([
+    'peerlibrary:assert',
+    'tinytest',
+    'test-helpers'
+  ], ['client', 'server']);
+
+  api.addFiles([
+    'tests.js'
+  ], ['client', 'server']);
 });
